a874464f43a5256940bddcb8c9627136f08da0f8
AlexLisborg <guslisboal@student.gu.se>
2023-02-28 22:44:40 +0100

    Guard Walking Animation Added

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -8,13 +8,25 @@ public class EnemyMovement : MonoBehaviour
 {
     public float speed = 3f;
     private Func<Vector2> target;
-    
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
     private void Update()
     {
         if (target != null)
         {
             float travel = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, target(), travel);
+            
+            Vector2 pos2d = new Vector2(transform.position.x, transform.position.y);
+            Vector2 _movement = (target() - pos2d).normalized;
+            _anim.SetFloat("Horizontal",_movement.x);
+            _anim.SetFloat("Vertical",_movement.y);
+            _anim.SetFloat("Speed",_movement.sqrMagnitude);
+
         }
     }
 
